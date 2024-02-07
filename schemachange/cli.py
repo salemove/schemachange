fc3b3b1219df94578272343af3e506bd02051737
stas <bondarenko.stanislav.96@gmail.com>
2024-02-07 12:03:18 +0200

    wip update requirements

--- schemachange/cli.py
+++ schemachange/cli.py
@@ -19,9 +19,9 @@ from cryptography.hazmat.primitives import serialization
 from jinja2.loaders import BaseLoader
 from pandas import DataFrame
 
-#region Global Variables 
+#region Global Variables
 # metadata
-_schemachange_version = '3.5.3'
+_schemachange_version = '3.5.4'
 _config_file_name = 'schemachange-config.yml'
 _metadata_database_name = 'METADATA'
 _metadata_schema_name = 'SCHEMACHANGE'
